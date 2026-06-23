23a4a93c861a3056ec561dd77d947f420603a465
Jake Wood <jzwood14@yahoo.com>
2026-06-23 14:06:39 -0400

    mvp parseElem

--- src/x1b/markup.ts
+++ src/x1b/markup.ts
@@ -1,16 +1,16 @@
 import {
   CURSOR,
   Cursor,
-  join,
-  map,
-  map3,
+  left,
+  map2,
   oneOf,
-  oneOrMore,
   Parser,
   right,
   satisfy,
+  someWhitespace,
   trimEnd,
   word,
+  wordBy,
   wrap,
   zeroOrMore,
 } from "../parser/index.ts";
@@ -55,13 +55,17 @@ interface Attribute {
   value: string;
 }
 
-function isAttrChar(grapheme: string) {
-  return (/^[a-z0-9#-]$/).test(grapheme);
-}
-function parseAttribute(key: string): Parser<Attribute> {
-  const parseValue = wrap('="', map(oneOrMore(satisfy(isAttrChar)), join), '"');
-  return map(right(word(key), parseValue), (value) => ({ key, value }));
-}
+const parseAttrKey: Parser<string> = wordBy((grapheme) =>
+  (/^[a-z-]$/).test(grapheme)
+);
+const parseAttrValue: Parser<string> = wordBy((grapheme) =>
+  (/^[a-z0-9#-]$/).test(grapheme)
+);
+const parseAttribute: Parser<Attribute> = map2(
+  right(someWhitespace, parseAttrKey),
+  wrap('="', parseAttrValue, '"'),
+  (key, value) => ({ key, value }),
+);
 
 interface Element {
   tag: TagName;
@@ -75,31 +79,34 @@ type TML = Node[];
 function parseCustomElem(tag: TagName): Parser<Element> {
   return (input: string, cursor: Cursor = CURSOR) => {
     const parseTag = word(tag);
-    const parseOpen: Parser<string> = wrap("<", parseTag, ">");
+    const parseOpen: Parser<Attribute[]> = wrap(
+      "<",
+      trimEnd(right(parseTag, zeroOrMore(parseAttribute))),
+      ">",
+    );
     const parseClose: Parser<string> = wrap("</", parseTag, ">");
-    const parser = map3(
+    const parser = map2(
       parseOpen,
-      parseML,
-      parseClose,
-      (_open, children, _close): Element => ({ tag, children }),
+      left(
+        parseML,
+        parseClose,
+      ),
+      (attrs, children): Element => ({ tag, attrs, children }),
     );
     return parser(input, cursor);
   };
 }
 
-const parseText: Parser<string> = map(
-  oneOrMore(satisfy((c) => c !== "<")),
-  join,
-);
+const parseText: Parser<string> = wordBy((c) => c !== "<");
 const parseElem: Parser<Element> = oneOf(
   ...Object.values(TagName).map(parseCustomElem),
 );
 const parseML: Parser<TML> = zeroOrMore(oneOf<Node>(parseElem, parseText));
 
 const input: string = `<box>
-  <b>header</b>
+  <b flow="auto" bg-color="#34eb0a">header</b>
   hello world
-  <box>my name is <s>jake</s> chipmunk</box>
+  <box>my name is <s font-color="#ddd">jake</s> chipmunk</box>
 </box>
 `;
 
